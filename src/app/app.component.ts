02eaeffb24d3d5531fad8d311d03a4a22e98a074
Benyiman <89081846+Benyiman@users.noreply.github.com>
2023-11-07 19:37:20 -0300

    07/11 containers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Practicas-Unach';
+  constructor(private router: Router) { }
+
+  // Función para redirigir al otro componente
+  redirigirPortada1() {
+    this.router.navigate(['app/Components/portada']); // Reemplaza 'otro-componente' con la ruta de tu componente de destino
+  }
 }
