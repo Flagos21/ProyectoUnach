1fb53a2b35516d54071b8ccfa37762abecc2036b
Fabian <flagosp21@gmail.com>
2023-11-21 16:37:36 -0300

    AdminRegister

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Services/auth.service';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent{
 
+  formLogin: FormGroup;
   formReg: FormGroup;
   errorMessage: string = '';
   successMessage: string = '';
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+
     this.formReg = new FormGroup({
       nombre: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -23,30 +33,44 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const signUpButton = this.el.nativeElement.querySelector('#signUp');
+    const signInButton = this.el.nativeElement.querySelector('#signIn');
+    const container = this.el.nativeElement.querySelector('#container');
 
-  async onSubmit() {
+    signUpButton.addEventListener('click', () =>
+      this.renderer.addClass(container, 'right-panel-active'));
+
+    signInButton.addEventListener('click', () =>
+      this.renderer.removeClass(container, 'right-panel-active'));
+  }
+
+  onSubmitLogin() {
+    this.authService.login(this.formLogin.value)
+      .then(response => {
+        console.log(response);
+        this.router.navigate(['/home']);
+      })
+      .catch(error => console.log(error));
+  }
+
+  async onSubmitRegister() {
     try {
-      // Registra al usuario con Firebase Authentication
       const userCredential = await this.authService.registerAndAddUser(this.formReg.value);
 
-      // Muestra un mensaje de éxito al usuario
       this.successMessage = 'Usuario registrado con éxito.';
 
       console.log('Usuario registrado con éxito. UID:', userCredential.user.uid);
-
-      // Redirige al usuario a otra página después del registro exitoso
-      // this.router.navigate(['/login']);
+      
     } catch (error: any) {
       console.error('Error al registrar usuario:', error);
 
       if (error.code === 'auth/email-already-in-use') {
-        this.errorMessage =
-          'El correo electrónico ya está en uso. Por favor, utiliza otro correo electrónico.';
+        this.errorMessage = 'El correo electrónico ya está en uso. Por favor, utiliza otro correo electrónico.';
       } else {
-        this.errorMessage =
-          'Hubo un error al registrar al usuario. Por favor, inténtalo de nuevo más tarde.';
+        this.errorMessage = 'Hubo un error al registrar al usuario. Por favor, inténtalo de nuevo más tarde.';
       }
     }
   }
 }
+
